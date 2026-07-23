4f4dc51b1f82f50c9282f9c6b22f963959233320
Moeen Ul Islam <139092562+moeen14@users.noreply.github.com>
2026-07-22 21:35:27 -0500

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyAh1WcBGIs1FPxflgIs45VikY5h0EhqavA",
-  authDomain: "phd-dashboard-8f480.firebaseapp.com",
-  projectId: "phd-dashboard-8f480",
-  storageBucket: "phd-dashboard-8f480.firebasestorage.app",
-  messagingSenderId: "971140364077",
-  appId: "1:971140364077:web:2f98757e6092c6ddeac5a4"
-};
